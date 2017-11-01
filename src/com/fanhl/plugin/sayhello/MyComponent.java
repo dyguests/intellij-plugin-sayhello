fb44c91c6bf10db8f3c566c3a4b5efd1cceca109
fanhl <fanhonglin@klicen.com>
2017-11-01 10:13:25 +0800

    Init Commit.

---
+++ src/com/fanhl/plugin/sayhello/MyComponent.java
@@ -0,0 +1,40 @@
+package com.fanhl.plugin.sayhello;
+
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * date 2017/11/01
+ *
+ * @author fanhl
+ */
+public class MyComponent implements ApplicationComponent {
+    public MyComponent() {
+    }
+
+    @Override
+    public void initComponent() {
+        // TODO: insert component initialization logic here
+    }
+
+    @Override
+    public void disposeComponent() {
+        // TODO: insert component disposal logic here
+    }
+
+    @Override
+    @NotNull
+    public String getComponentName() {
+        return "MyComponent";
+    }
+
+    public void sayHello() {
+        // Show dialog with message
+        Messages.showMessageDialog(
+                "Hello World!",
+                "Sample",
+                Messages.getInformationIcon()
+        );
+    }
+}
